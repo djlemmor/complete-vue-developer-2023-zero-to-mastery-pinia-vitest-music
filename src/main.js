4879860a4b94cb1bb494228d95c3bf25576c345d
djlemmor <lemmornuique@gmail.com>
2023-04-07 02:45:27 +0800

    Section 17: Master Project: PWA (Progressive Web App)

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ import './assets/base.css'
 
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 let app
 
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Music App',
+        theme_color: '#ff5e3a',
+        icons: [
+          {
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
